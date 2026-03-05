51d17067193e76b8369598f4b7131bf1ac0b3d75
Mayuresh Jadhav <mj.ethical@gmail.com>
2026-03-05 09:05:32 +0530

    intial

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.redirect("/home.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "signup.html"));
+});
+
+app.get("/tournaments", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "tournaments.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+app.post("/login", (req, res) => {
+  res.redirect("/home.html");
+});
+
+app.get('/tournaments', (req, res) => {
+    res.sendFile(__dirname + '/public/tournaments.html');
+});
